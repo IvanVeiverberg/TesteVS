84687370930dc13e2f058843f60aa334e7464376
Ivan Veiverberg <ivanveiverberg@hotmail.com>
2020-05-25 09:45:02 -0300

    asdasdas

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,8 +13,6 @@ namespace ConsoleApp1
             Console.WriteLine("Teste1");
             Console.WriteLine("Teste2");
             Console.WriteLine("Teste3");
-            Console.WriteLine("Teste4");
-            Console.WriteLine("Teste5");
         }
     }
 }
